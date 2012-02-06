577b3d01ebdd27661511c6ebb50a0fd296c6ae7e
Dan Lynch <pyramation@gmail.com>
2012-02-05 23:36:32 -0800

    added playlist server side

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 var io = require('socket.io');
+var fs = require('fs');
 
 var app = io.listen(7777);
 
@@ -7,6 +8,13 @@ app.sockets.on('connection', function(socket) {
         console.log('playing ...');
         console.log(data);
         socket.broadcast.emit('play', data);
-    })
+    });
+
+    socket.on('list', function list(data) {
+
+        var d = fs.readFileSync('server/playlist.json');
+        var packet = JSON.parse(d);
+        socket.emit('list', packet);
+    }); 
 
 });
